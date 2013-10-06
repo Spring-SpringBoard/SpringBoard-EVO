5cab5121ef266654de6483c74dd094a972d22f43
gajop <gajo01@gmail.com>
2013-10-06 13:57:22 +0200

    initial commit - game starts but has bugs

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+  name='Scenario Editor EVO',
+  description='Scenario Editor for Evolution RTS',
+  shortname='SE_EVO',
+  version='0.1',
+  mutator='Official',
+  game='Scenario Editor EVO',
+  shortGame='SE_EVO',
+  modtype=1,
+  depend = {
+	'Scenario Editor Core 0.2',
+	'Evolution RTS - v6.51'
+  },
+}
