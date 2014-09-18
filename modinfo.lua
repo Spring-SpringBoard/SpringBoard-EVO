a79f3b2d81b0d4c5e27a51a1a56c8267c5cf379c
Gajo Petrovic <gajop@uns.ac.rs>
2014-09-18 22:57:16 +0200

    updated to git version

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
   name='Scenario Editor EVO',
   description='Scenario Editor for Evolution RTS',
   shortname='SE_EVO',
-  version='0.1',
+  version='git',
   mutator='Official',
   game='Scenario Editor EVO',
   shortGame='SE_EVO',
   modtype=1,
   depend = {
-	'Scenario Editor Core 0.2',
-	'Evolution RTS - v6.51'
+	'Scenario Editor Core git',
+	'Evolution RTS - v8.02'
   },
 }
