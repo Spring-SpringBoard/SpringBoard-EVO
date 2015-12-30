99dd9f1ce55e0204a542bbe6ebd09c6998011a0d
Gajo Petrovic <gajo01@gmail.com>
2015-12-31 02:23:39 +0900

    update evo/scened dependencies

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,14 @@
 return {
-  name='Scenario Editor EVO',
-  description='Scenario Editor for Evolution RTS',
+  name='Scened EVO',
+  description='Scened for Evolution RTS',
   shortname='SE_EVO',
   version='git',
   mutator='Official',
-  game='Scenario Editor EVO',
+  game='Scened EVO',
   shortGame='SE_EVO',
   modtype=1,
   depend = {
-	'Scenario Editor Core git',
-	'Evolution RTS - v8.02'
+	'Scened Core git',
+	'Evolution RTS - v9.01'
   },
 }
